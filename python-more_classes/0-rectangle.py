f818e42dd2d5e3abd26ddf6d0f8f2982f4cc575d
gulnisahlb <gulnisahlb@gmail.com>
2025-11-24 16:18:03 +0000

    hlb

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines an empty Rectangle class."""
+
+
 class Rectangle:
-    """This module defines an empty Rectangle class."""
+    """Empty class that defines a rectangle."""
     pass
