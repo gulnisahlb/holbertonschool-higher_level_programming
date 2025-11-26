d9cbd4aaa1c1fc67f40c165590288802b63f9272
gulnisahlb <gulnisahlb@gmail.com>
2025-11-26 13:31:01 +0000

    hlb

---
+++ python-data_structures/2-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)  # [1, 2, 3, 9, 5]
+print(my_list)   # [1, 2, 3, 9, 5]
