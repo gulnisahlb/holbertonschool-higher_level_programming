de25910eb6512e264ce4acb25e8b1fe3839344eb
gulnisahlb <gulnisahlb@gmail.com>
2025-11-26 08:21:28 +0000

    hlb

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,9 +7,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
+
     if op == "+":
         result = add(a, b)
     elif op == "-":
@@ -21,4 +23,5 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
     print("{} {} {} = {}".format(a, op, b, result))
