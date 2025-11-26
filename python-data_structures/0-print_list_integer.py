13a8590abacf4b5b0a47df8d1c5997ba9c2e434f
gulnisahlb <gulnisahlb@gmail.com>
2025-11-26 12:46:24 +0000

    hlb

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Print all integers of a list"""
+    """Print all integers of a list, one per line, using str.format()"""
     for num in my_list:
-        print(str(num))
+        print("{:d}".format(num))
