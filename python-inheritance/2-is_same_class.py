0389e94cd59abf8047a74ab8050fe8f028cdb46c
gulnisahlb <gulnisahlb@gmail.com>
2025-11-30 17:13:28 +0000

    hlb

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""hello world"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if obj is exactly an instance of a_class, else False.
+
+    Args:
+        obj: object to check
+        a_class: class to compare
+
+    Returns:
+        True if obj is exactly instance of a_class, False otherwise
+    """
+    return type(obj) == a_class
