9a311ad509d491a295cda8d2a0e3b8f501f36777
gulnisahlb <gulnisahlb@gmail.com>
2025-12-08 21:22:14 +0400

    file

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- this is comment 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+--this is using for use db 
 USE hbtn_0d_usa;
+--table creating
 CREATE TABLE IF NOT EXISTS states(
     id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     name VARCHAR(256) NOT NULL
