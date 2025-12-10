272371cc337f02bb52422ec50b9fdfe884bd01f0
gulnisahlb <gulnisahlb@gmail.com>
2025-12-10 10:32:14 +0000

    hlb

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+from urllib import request, parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = parse.urlencode({'email': email}).encode('utf-8')
+    req = request.Request(url, data=data, method='POST')
+    with request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
