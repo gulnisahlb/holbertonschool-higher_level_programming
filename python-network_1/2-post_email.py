08b7c57516f0e071f33075c74a7fbe7e75d15887
gulnisahlb <gulnisahlb@gmail.com>
2025-12-10 10:34:55 +0000

    hlb

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""this is docstr
+"""
+
+import urllib.parse
+import urllib.request
 import sys
-from urllib import request, parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = parse.urlencode({'email': email}).encode('utf-8')
-    req = request.Request(url, data=data, method='POST')
-    with request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    req = urllib.request.Request(url, data=data, method="POST")
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
