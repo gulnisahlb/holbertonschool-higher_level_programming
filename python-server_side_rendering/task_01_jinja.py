7fb4cef01ad890c48249f49eb0cefbfa1f2bdd10
gulnisahlb <gulnisahlb@gmail.com>
2025-12-10 17:01:28 +0000

    hlb

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
-
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -13,6 +13,5 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
